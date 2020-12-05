89e8ea65735ebff8ea2e98bf13fd7cd5006a29e3
Sam Moore <sam@paradigmpost.com>
2020-12-05 06:13:46 +0000

    day 5

--- Sources/aoc/day5.swift
+++ Sources/aoc/day5.swift
@@ -1,5 +1,75 @@
 import Foundation
 
-func day5(input: String) -> Int {
-  fatalError()
+enum FrontBack: Character {
+  case Front = "F"
+  case Back = "B"
+}
+
+enum LeftRight: Character {
+  case Left = "L"
+  case Right = "R"
+}
+
+struct SeatPlan {
+  private var data: [[Int]]
+
+  var ids: [Int] { data.flatMap { $0 }.filter { $0 >= 0 } }
+
+  var mine: Int? {
+    let sorted = ids.sorted()
+    // not efficient
+    for (index, value) in sorted.enumerated() {
+      if 0 ..< sorted.count ~= index + 1 && sorted[index + 1] - 2 == value {
+        return value + 1
+      }
+    }
+    return nil
+  }
+
+  init() {
+    self.data = Array(repeating: Array(repeating: -1, count: 8), count: 128)
+  }
+
+  private init(data: [[Int]]) {
+    self.data = data
+  }
+
+  func populateSeat(_ encoded: String) -> SeatPlan {
+    var i = 128
+    var j = 8
+    var y = 0
+    var x = 0
+
+    for char in encoded {
+      guard i > 0 && j > 0 else {
+        fatalError("your logic might be flawed")
+      }
+
+      if let char = FrontBack(rawValue: char) {
+        i /= 2
+        if char == .Back { y += i }
+      }
+      else if let char = LeftRight(rawValue: char) {
+        j /= 2
+        if char == .Right { x += j }
+      }
+      else {
+        break
+      }
+    }
+
+    var newData = data
+    newData[y][x] = (y * 8) + x
+    return SeatPlan(data: newData)
+  }
+}
+
+func day5(input: String) -> (Int, Int?) {
+  let plan = input
+    .components(separatedBy: "\n")
+    .reduce(SeatPlan()) { (plan, seat) in
+      plan.populateSeat(seat)
+    }
+
+  return (plan.ids.max()!, plan.mine)
 }
--- Sources/aoc/main.swift
+++ Sources/aoc/main.swift
@@ -3,7 +3,7 @@ import Foundation
 let filePath: String = Bundle.module.path(forResource: "5", ofType: "txt")!
 let contents = String(decoding: try! Data(contentsOf: URL(fileURLWithPath: filePath)), as: UTF8.self)
 
-var answer: Int? = day5(input: contents)
+var answer: Any? = day5(input: contents)
 
 guard let answer = answer else {
   fatalError("no match found")
