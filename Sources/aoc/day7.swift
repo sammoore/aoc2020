8f6936e9aefb172c312b43c8c9feb56b7ec3f65b
Sam Moore <sam@paradigmpost.com>
2020-12-08 07:22:11 +0000

    fix day 7 part 1 speed

--- Sources/aoc/day7.swift
+++ Sources/aoc/day7.swift
@@ -6,7 +6,8 @@ extension Int {
   }
 }
 
-func rule(_ encoded: String) -> (String, [String]) {
+// if `count` is true, this is too expensive to run for all rules
+func rule(_ encoded: String, count: Bool = false) -> (String, [(Int, String)]) {
   let parts = encoded.components(separatedBy: " bags contain ")
   
   return (parts.first!, parts.last!
@@ -19,34 +20,39 @@ func rule(_ encoded: String) -> (String, [String]) {
       } else {
         let count = Int(encodedCount)!
         let type = String(each.split(separator: " ").dropFirst().joined(separator: " "))
-        return requirements + count.of(type)
+        return requirements + [(count, type)]
       }
     }
   )
 }
 
-func contents(of: String, given rules: Dictionary<String, [String]>) -> [String] {
-  return rules[of]!.reduce(rules[of]!) { (out, each) in
-    out + contents(of: each, given: rules)
-  }
-}
-
-func day7_1(input: String) -> Int {
-  let rules: Dictionary<String, [String]> = input
+func parseRules(input: String) -> Dictionary<String, [(Int, String)]> {
+  return input
     .components(separatedBy: "\n")
     .reduce([:]) { (out, each) in
       guard each != "" else { return out }
       let (type, requirements) = rule(each)
       return out.merging([type: requirements]) { $1 }
     }
+}
 
-  let contentsByOutermostBag: Dictionary<String, [String]> = rules.keys
+func contents(of: String, given rules: Dictionary<String, [(Int, String)]>) -> [(Int, String)] {
+  return rules[of]!.reduce(rules[of]!) { (out, each) in
+    out + contents(of: each.1, given: rules)
+  }
+}
+
+func day7_1(input: String) -> Int {
+  let rules: Dictionary<String, [(Int, String)]> = parseRules(input: input)
+
+  let contentsByOutermostBag: Dictionary<String, [(Int, String)]> = rules.keys
     .reduce([:]) { (out, type) in
       return out.merging([type: contents(of: type, given: rules)]) { $1 }
     }
 
   return contentsByOutermostBag
-    .filter { (key, bags) in bags.contains("shiny gold") }
+    .filter { (key, bags) in bags.filter { $0.1 == "shiny gold" }.count > 0 }
     .keys
     .count
 }
+
